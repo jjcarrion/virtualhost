9987a1ef307953e417d47d9c5f4f38b90d2986db
Jose Jiménez <jose.jimenez@wunderkraut.com>
2015-02-15 20:06:07 +0100

    Changed to my configuration

--- virtualhost.sh
+++ virtualhost.sh
@@ -5,13 +5,14 @@ TEXTDOMAIN=virtualhost
 ### Set default parameters
 action=$1
 domain=$2
-rootdir=$3
+name=$3
+rootdir=$4
+destiny=$5
+ip=$6
 owner=$(who am i | awk '{print $1}')
 email='webmaster@localhost'
 sitesEnable='/etc/apache2/sites-enabled/'
 sitesAvailable='/etc/apache2/sites-available/'
-userDir='/var/www/'
-sitesAvailabledomain=$sitesAvailable$domain.conf
 
 ### don't modify from here unless you know what you are doing ####
 
@@ -32,9 +33,33 @@ do
 	read  domain
 done
 
-if [ "$rootdir" == "" ]; then
-	rootdir=${domain//./}
+if [ "$destiny" == "" ]; then
+	echo -e $"Please provide a destiny, '/srv/www/' or '~/projects'"
+	read  destiny
 fi
+
+if [ "$name" == "" ]; then
+	echo -e $"Please provide a simple name, for example domain"
+	read  name
+fi
+
+if [ "$rootdir" == "" ]; then
+	echo -e $"Please provide a rootdir, for example www or web or docroot"
+	read  rootdir
+fi
+
+if [ "$ip" == "" ]; then
+	echo -e $"Please provide an IP, 127.0.0.1"
+	read  ip
+fi
+
+sitesAvailabledomain=$sitesAvailable$name.conf
+completepath=$destiny/$name/$rootdir
+echo $sitesAvailable \n
+echo $sitesAvailabledomain \n
+echo $completepath
+
+#destiny='/var/www/'
 
 if [ "$action" == 'create' ]
 	then
@@ -45,40 +70,40 @@ if [ "$action" == 'create' ]
 		fi
 
 		### check if directory exists or not
-		if ! [ -d $userDir$rootdir ]; then
-			### create the directory
-			mkdir $userDir$rootdir
-			### give permission to root dir
-			chmod 755 $userDir$rootdir
-			### write test file in the new domain dir
-			if ! echo "<?php echo phpinfo(); ?>" > $userDir$rootdir/phpinfo.php
-			then
-				echo $"ERROR: Not able to write in file $userDir/$rootdir/phpinfo.php. Please check permissions"
-				exit;
-			else
-				echo $"Added content to $userDir$rootdir/phpinfo.php"
-			fi
-		fi
+#		if ! [ -d $destiny$rootdir ]; then
+#			### create the directory
+#			mkdir $destiny$rootdir
+#			### give permission to root dir
+#			chmod 755 $destiny$rootdir
+#			### write test file in the new domain dir
+#			if ! echo "<?php echo phpinfo(); ?>" > $destiny$rootdir/phpinfo.php
+#			then
+#				echo $"ERROR: Not able to write in file $destiny/$rootdir/phpinfo.php. Please check permissions"
+#				exit;
+#			else
+#				echo $"Added content to $destiny$rootdir/phpinfo.php"
+#			fi
+#		fi
 
 		### create virtual host rules file
 		if ! echo "
-		<VirtualHost *:80>
-			ServerAdmin $email
-			ServerName $domain
-			ServerAlias $domain
-			DocumentRoot $userDir$rootdir
-			<Directory />
-				AllowOverride All
-			</Directory>
-			<Directory $userDir$rootdir>
-				Options Indexes FollowSymLinks MultiViews
-				AllowOverride all
-				Require all granted
-			</Directory>
-			ErrorLog /var/log/apache2/$domain-error.log
-			LogLevel error
-			CustomLog /var/log/apache2/$domain-access.log combined
-		</VirtualHost>" > $sitesAvailabledomain
+<VirtualHost *:80>
+	ServerAdmin $email
+	ServerName $domain
+	ServerAlias $domain
+	DocumentRoot $completepath
+	<Directory />
+		AllowOverride All
+	</Directory>
+	<Directory $completepath>
+		Options Indexes FollowSymLinks MultiViews
+		AllowOverride all
+		Require all granted
+	</Directory>
+	ErrorLog /var/log/apache2/$name-error.log
+	LogLevel error
+	CustomLog /var/log/apache2/$name-access.log combined
+</VirtualHost>" > $sitesAvailabledomain
 		then
 			echo -e $"There is an ERROR creating $domain file"
 			exit;
@@ -87,7 +112,7 @@ if [ "$action" == 'create' ]
 		fi
 
 		### Add domain in /etc/hosts
-		if ! echo "127.0.0.1	$domain" >> /etc/hosts
+		if ! echo "$ip	$domain" >> /etc/hosts
 		then
 			echo $"ERROR: Not able to write in /etc/hosts"
 			exit;
@@ -95,11 +120,11 @@ if [ "$action" == 'create' ]
 			echo -e $"Host added to /etc/hosts file \n"
 		fi
 
-		if [ "$owner" == ""  ]; then
-			chown -R $(whoami):$(whoami) $userDir$rootdir
-		else
-			chown -R $owner:$owner $userDir$rootdir
-		fi
+#		if [ "$owner" == ""  ]; then
+#			chown -R $(whoami):$(whoami) $destiny$rootdir
+#		else
+#			chown -R $owner:$owner $destiny$rootdir
+#		fi
 
 		### enable website
 		a2ensite $domain
@@ -108,9 +133,12 @@ if [ "$action" == 'create' ]
 		/etc/init.d/apache2 reload
 
 		### show the finished message
-		echo -e $"Complete! \nYou now have a new Virtual Host \nYour new host is: http://$domain \nAnd its located at $userDir$rootdir"
+		echo -e $"Complete! \nYou now have a new Virtual Host \nYour new host is: http://$domain \nAnd its located at $destiny$rootdir"
 		exit;
 	else
+
+		###### Delete #######
+
 		### check whether domain already exists
 		if ! [ -e $sitesAvailabledomain ]; then
 			echo -e $"This domain does not exist.\nPlease try another one"
@@ -131,13 +159,13 @@ if [ "$action" == 'create' ]
 		fi
 
 		### check if directory exists or not
-		if [ -d $userDir$rootdir ]; then
+		if [ -d $destiny$rootdir ]; then
 			echo -e $"Delete host root directory ? (y/n)"
 			read deldir
 
 			if [ "$deldir" == 'y' -o "$deldir" == 'Y' ]; then
 				### Delete the directory
-				rm -rf $userDir$rootdir
+				rm -rf $destiny$rootdir
 				echo -e $"Directory deleted"
 			else
 				echo -e $"Host directory conserved"
